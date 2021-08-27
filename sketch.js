bfa6fe0191667b74914cdd13db95dbce9da6a1c7
whitehat-G6 <88480121+WHJR-G6@users.noreply.github.com>
2021-08-27 23:22:33 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -28,12 +28,8 @@ function preload(){
 function setup() {
   createCanvas(600, 350);
   
-  mario = createSprite(50,295,20,50);
-  
-  mario.addAnimation("running",mario_running);
-  mario.addAnimation("collided", mario_collided);
-  mario.scale = 2;
-  mario.debug=true
+
+ 
   ground = createSprite(200,330,400,20);
   ground.addImage("ground",groundImage);
   ground.x = ground.width /2;
@@ -57,19 +53,19 @@ function draw() {
     ground.velocityX = -(12);
   
     if(keyDown("space") && mario.y >= 250) {
-      mario.velocityY = -12;
+   
       jumpSound.play();
     }
   if(score>0 && score%10 === 0){
        checkPointSound.play() 
     }
-    mario.velocityY = mario.velocityY + 0.5
+  
     
     if (ground.x < 0){
       ground.x = ground.width/2;
     }
 
-    mario.collide(invisibleGround);
+   
     spawnbricks(); 
   
   drawSprites();
@@ -87,12 +83,7 @@ function spawnbricks() {
     
      //assign lifetime to the variable
     brick.lifetime = 200;
-    
-    //adjust the depth
-    brick.depth = mario.depth;
-    mario .depth = mario.depth + 1;
-    
-    
+      
   }
   
 }
