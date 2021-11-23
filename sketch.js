2080056898e6cc625ef079b15f487584a851de38
whitehat-G6 <88480121+WHJR-G6@users.noreply.github.com>
2021-11-23 13:14:54 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,87 +1,156 @@
+const Engine = Matter.Engine;
+const Render = Matter.Render;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+const Body = Matter.Body;
+const Composites = Matter.Composites;
+const Composite = Matter.Composite;
 
 
-var mario, mario_running, mario_collided;
-var ground, invisibleGround, groundImage;
+let engine;
+let world;
+var plank;
+var ground;
+var higherground;
+var con;
+var con2;
+var rope;
+var bubble,bubble_img;
 
-var bricksGroup, brickImage;
-var obstaclesGroup, obstacle1, obstacle2, obstacle3, obstacle4, obstacle5, obstacle6;
+function preload()
+{
+  bubble_img = loadImage("bubble.png")
+  bg_img = loadImage('background.png');
+  food = loadImage('melon.png');
+  rabbit = loadImage('Rabbit-01.png');
 
-var score=0;
-
-function preload(){
-  bg=loadImage("bg.png")
-  mario_running =   loadAnimation("mario00.png","mario01.png","mario02.png","mario03.png");
-  mario_collided = loadAnimation("collided.png");
+  blink = loadAnimation("blink_1.png","blink_2.png","blink_3.png");
+  eat = loadAnimation("eat_0.png" , "eat_1.png","eat_2.png","eat_3.png","eat_4.png");
+  sad = loadAnimation("sad_1.png","sad_2.png","sad_3.png");
+  star_img = loadImage('star.png');
   
-  groundImage = loadImage("ground2.png");
-  
-  obstacleimage = loadAnimation("obstacle1.png","obstacle2.png","obstacle3.png","obstacle4.png");
-  
-  gameOverImg = loadImage("gameOver.png");
-  restartImg = loadImage("restart.png");
-  
-  jumpSound = loadSound("jump.mp3")
-  dieSound = loadSound("die.mp3")
-  checkPointSound = loadSound("checkPoint.mp3")
+  blink.playing = true;
+  eat.playing = true;
+  sad.playing = true;
+  sad.looping= false;
+  eat.looping = false; 
 }
 
 function setup() {
-  createCanvas(600, 350);
-  
+  createCanvas(500,800);
+  frameRate(80);
+  engine = Engine.create();
+  world = engine.world;
 
- 
-  ground = createSprite(200,330,400,20);
-  ground.addImage("ground",groundImage);
-  ground.x = ground.width /2;
-  ground.velocityX = -(2);
-  
-  invisibleGround = createSprite(200,300,400,10);
-  invisibleGround.visible = false;
-  
-  
-  fill(0);
-textSize(24);
-textFont('Georgia');
-  score = 0;
-}
-
-function draw() {
-  //trex.debug = true;
-  background(bg);
-  text("Score: "+ score, 480,30);
-    
-    ground.velocityX = -(12);
-  
-   
-  if(score>0 && score%10 === 0){
-       checkPointSound.play() 
-    }
-  
-    
-    if (ground.x < 0){
-      ground.x = ground.width/2;
-    }
-
-   
-    spawnbricks(); 
-  
-  drawSprites();
-}
-
-function spawnbricks() {
-  //write code here to spawn the brick
-  if (frameCount % 60 === 0) {
-    var brick = createSprite(600,120,40,10);
-    brick.debug=true
-    brick.y = Math.round(random(150,180));
-    brick.scale = 1;
-    brick.velocityX = -3;
-    
-     //assign lifetime to the variable
-    brick.lifetime = 200;
-      
+   var fruit_options = {
+    restitution: 0.8
   }
   
+  ground =new Ground(250,height-10,width,20);
+  fruit = Bodies.circle(100,400,15,fruit_options);
+  World.add(world,fruit);
+  
+  bubble = createSprite(290,460,20,20);
+  bubble.addImage(bubble_img);
+  bubble.scale = 0.1;
+  
+  //bunny sprite
+  blink.frameDelay = 20;
+  eat.frameDelay = 20;
+  bunny = createSprite(270,100,100,100);
+  bunny.addImage(rabbit);
+  bunny.scale = 0.2;
+  higherground =new Ground(300,170,100,10);
+
+  bunny.addAnimation('blinking',blink);
+  bunny.addAnimation('eating',eat);
+  bunny.addAnimation('crying',sad);
+  bunny.changeAnimation('blinking');
+
+  rope = new Rope(4,{x:230,y:330});
+  rope2 = new Rope(4,{x:50,y:450});
+  con = new Link(rope,fruit);
+  con2 = new Link(rope2,fruit);
+
+  //btn 1
+  button = createImg('cut_btn.png');
+  button.position(200,320);
+  button.size(50,50);
+
+  button2 = createImg('cut_btn.png');
+  button2.position(30,420);
+  button2.size(50,50);
+  button2.mouseClicked(drop);
+
+  ellipseMode(RADIUS);
 }
 
+function draw() 
+{
+  background(51);
+  image(bg_img,0,0,width,height);
+  Engine.update(engine);
+  
+  push();
+  imageMode(CENTER);
+  if(fruit!=null){
+    image(food,fruit.position.x,fruit.position.y,70,70);
+  }
+  pop();
+
+  ground.show();
+  higherground.show();
+  rope.show();
+  rope2.show();
+
+  if(collide(fruit,bunny,80)==true)
+  {
+   remove_rope();
+   bubble.visible = false;
+    World.remove(engine.world,fruit);
+    fruit = null;
+    bunny.changeAnimation('eating');
+  }
+  
+  if(collide(fruit,bubble,40) == true)
+    {
+      engine.world.gravity.y = -1;
+      bubble.position.x = fruit.position.x;
+      bubble.position.y = fruit.position.y;
+    }
+
+  drawSprites();
+
+}
+
+function drop()
+{
+  rope2.break();
+  con2.dettach();
+  con2 = null; 
+}
+
+function remove_rope()
+{
+  rope.break();
+  con.dettach();
+  con = null; 
+}
+
+function collide(body,sprite,x)
+{
+  if(body!=null)
+        {
+         var d = dist(body.position.x,body.position.y,sprite.position.x,sprite.position.y);
+          if(d<=x)
+            {
+              
+               return true; 
+            }
+            else{
+              return false;
+            }
+         }
+}
 
